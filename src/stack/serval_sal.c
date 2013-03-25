b280e883a5166ac5e03800eb408db5da513d04ba
Simon Peter <simpeter@cs.washington.edu>
2013-03-25 14:12:53 -0700

    sadf

--- src/stack/serval_sal.c
+++ src/stack/serval_sal.c
@@ -4243,7 +4243,7 @@ int serval_sal_transmit_skb(struct sock *sk, struct sk_buff *skb,
                         ssk->af_ops->send_check(sk, cskb);
                 }
 
-                /* PRINTK("Sending off with TaaS authenticator %llu\n", target->taas_auth); */
+                PRINTK("Sending off with TaaS authenticator %llu\n", target->taas_auth);
 
                 /* Add SAL header */
                 /* sh = serval_sal_build_header(sk, cskb, target->taas_auth); */
