2ff5c3bf8f2481bdea4e0d969cb5ef7f8fac479c
Simon Peter <simpeter@cs.washington.edu>
2013-03-24 21:00:29 -0700

    asdf

--- src/stack/serval_sal.c
+++ src/stack/serval_sal.c
@@ -4041,7 +4041,7 @@ int serval_sal_transmit_skb(struct sock *sk, struct sk_buff *skb,
          * instead, which is released in the skb's destructor.
          */
 
-#if 0
+#if 1
         /* If we are connected, transmit immediately */
         if ((1 << sk->sk_state) & (SALF_CONNECTED | 
                                    SALF_FINWAIT1 | 
@@ -4059,7 +4059,7 @@ int serval_sal_transmit_skb(struct sock *sk, struct sk_buff *skb,
         }
 #endif
 
-#if 0
+#if 1
 	/* Use service id to resolve IP, unless IP is already set. */
         if (memcmp(&zero_addr, 
                    &inet_sk(sk)->inet_daddr, 
